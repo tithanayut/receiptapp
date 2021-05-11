9058e28694c2ca1ffdb7d3689b8e04575d527ef0
Thanayut T <b@thanayut.in.th>
2021-05-11 20:59:51 +0700

    Update navbar to based on login status

--- components/Layout/NavBar.js
+++ components/Layout/NavBar.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/client";
 
 const NavBar = () => {
+	const [session] = useSession();
+
 	return (
 		<nav>
 			<div className="flex justify-between items-center px-5 py-3 bg-theme-main text-theme-opposite">
@@ -8,30 +11,49 @@ const NavBar = () => {
 					<Link href="/">
 						<a className="text-xl font-bold mr-6">Receiptapp</a>
 					</Link>
-					<ul className="flex">
-						<li className="mr-4">
-							<Link href="/payments">
-								<a>Payments</a>
-							</Link>
-						</li>
-						<li className="mr-4">
-							<Link href="/payers">
-								<a>Payers</a>
-							</Link>
-						</li>
-						<li className="mr-4">
-							<Link href="/items">
-								<a>Items</a>
-							</Link>
-						</li>
-					</ul>
+					{session && (
+						<ul className="flex">
+							<li className="mr-4">
+								<Link href="/payments">
+									<a>Payments</a>
+								</Link>
+							</li>
+							<li className="mr-4">
+								<Link href="/payers">
+									<a>Payers</a>
+								</Link>
+							</li>
+							<li className="mr-4">
+								<Link href="/items">
+									<a>Items</a>
+								</Link>
+							</li>
+						</ul>
+					)}
 				</div>
 				<ul className="flex">
-					<li>
-						<Link href="/login">
-							<a>Logout</a>
-						</Link>
-					</li>
+					{!session && (
+						<li>
+							<Link href="/login">
+								<a>Login</a>
+							</Link>
+						</li>
+					)}
+					{session && (
+						<li>
+							<p
+								className="cursor-pointer"
+								onClick={() =>
+									signOut({
+										redirect: false,
+										callbackUrl: "/",
+									})
+								}
+							>
+								Logout
+							</p>
+						</li>
+					)}
 				</ul>
 			</div>
 		</nav>
