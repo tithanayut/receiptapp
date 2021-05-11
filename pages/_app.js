ebc7294052cb5adcb5608118ced61fd0bc25e04d
Thanayut T <b@thanayut.in.th>
2021-05-11 18:24:56 +0700

    Add app favicon and Head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
 	return (
 		<Layout>
+			<Head>
+				<title>Receiptapp</title>
+				<link rel="icon" href="/favicon.svg" />
+			</Head>
+
 			<Component {...pageProps} />
 		</Layout>
 	);
