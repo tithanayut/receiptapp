ae10c7383d3d87d55dab975ea7b30bd3daf9d84a
Thanayut T <b@thanayut.in.th>
2021-05-11 20:51:40 +0700

    Add nextauth session provider to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
 import Head from "next/head";
+import { Provider } from "next-auth/client";
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
 	return (
-		<Layout>
+		<Provider session={pageProps.session}>
 			<Head>
 				<title>Receiptapp</title>
 				<link rel="icon" href="/favicon.svg" />
 			</Head>
-
-			<Component {...pageProps} />
-		</Layout>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
 	);
 }
 
