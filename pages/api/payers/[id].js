d8c2f67839eed9d27f44c4dfb6994bd2142b763d
Thanayut T <b@thanayut.in.th>
2021-05-16 23:35:56 +0700

    Create support for PUT in payer API

--- pages/api/payers/[id].js
+++ pages/api/payers/[id].js
@@ -31,6 +31,34 @@ const handler = async (req, res) => {
 			return res.status(404).json({ errors: ["Payer not found"] });
 		}
 		return res.status(200).json(data);
+	} else if (req.method === "PUT") {
+		if (!req.body.name || !req.body.notes) {
+			return res
+				.status(400)
+				.json({ errros: ["Request body not complete"] });
+		}
+
+		const prisma = new PrismaClient();
+		let data;
+		try {
+			data = await prisma.payer.update({
+				where: {
+					id,
+				},
+				data: {
+					name: req.body.name,
+					notes: req.body.notes,
+				},
+			});
+		} catch {
+			return res
+				.status(500)
+				.json({ errors: ["Database connection failed"] });
+		} finally {
+			await prisma.$disconnect();
+		}
+
+		return res.status(200).json({ message: "Updated", data });
 	}
 
 	return res.status(405).json({ errors: ["Method not supported"] });
