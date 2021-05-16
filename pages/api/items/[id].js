17ab25a361dc1dcf940b830f402749645a2fc30c
Thanayut T <b@thanayut.in.th>
2021-05-16 23:39:41 +0700

    Create support for PUT in item API

--- pages/api/items/[id].js
+++ pages/api/items/[id].js
@@ -31,6 +31,39 @@ const handler = async (req, res) => {
 			return res.status(404).json({ errors: ["Item not found"] });
 		}
 		return res.status(200).json(data);
+	} else if (req.method === "PUT") {
+		if (
+			!req.body.name ||
+			typeof req.body.price !== "number" ||
+			typeof req.body.allowAdjustPrice === "undefined"
+		) {
+			return res
+				.status(400)
+				.json({ errros: ["Request body not complete or is invalid"] });
+		}
+
+		const prisma = new PrismaClient();
+		let data;
+		try {
+			data = await prisma.item.update({
+				where: {
+					id,
+				},
+				data: {
+					name: req.body.name,
+					price: req.body.price,
+					allowAjustPrice: req.body.allowAdjustPrice,
+				},
+			});
+		} catch {
+			return res
+				.status(500)
+				.json({ errors: ["Database connection failed"] });
+		} finally {
+			await prisma.$disconnect();
+		}
+
+		return res.status(200).json({ message: "Updated", data });
 	}
 
 	return res.status(405).json({ errors: ["Method not supported"] });
