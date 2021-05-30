a231c1f501d207dbeb9101ff095f627cdb4a7125
Thanayut T <b@thanayut.in.th>
2021-05-30 20:19:08 +0700

    Create A5 receipt

---
+++ public/html/receipt/js/receipt.js
@@ -0,0 +1,57 @@
+async function fetchConfig() {
+	const res = await fetch("/api/config");
+	const data = await res.json();
+	return data;
+}
+
+async function fetchPayment(id) {
+	const res = await fetch("/api/payments/" + encodeURIComponent(id));
+	const data = await res.json();
+	return data;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+	// Fetch receipt config and fill fields
+	const config = await fetchConfig();
+	document.getElementById("orgName").textContent = config.receiptConfig.orgName;
+	document.getElementById("orgAddress").textContent =
+		config.receiptConfig.orgAddress;
+	document.getElementById("orgContact").textContent =
+		config.receiptConfig.orgContact;
+
+	// Fetch payment detail
+	const urlParams = new URLSearchParams(window.location.search);
+	const paymentId = urlParams.get("id");
+	if (!paymentId) {
+		window.location.href = "/";
+	}
+
+	const payment = await fetchPayment(paymentId);
+	const paymentDate = new Date(payment.date);
+	const paymentDateText =
+		paymentDate.getDate() +
+		"/" +
+		(paymentDate.getMonth() + 1) +
+		"/" +
+		paymentDate.getFullYear();
+
+	// Fill fields
+	document.getElementById("paymentId").textContent = payment.id;
+	document.getElementById("payerId").textContent = payment.Payer.id;
+	document.getElementById("payerName").textContent = payment.Payer.name;
+	document.getElementById("payerNotes").textContent = payment.Payer.notes;
+	document.getElementById("appUserDisplayname").textContent =
+		payment.AppUser.displayname;
+	document.getElementById("paymentDate").textContent = paymentDateText;
+
+	if (payment.PaymentDetail.length > 10) {
+		document.getElementById("payment-error").textContent =
+			"Length of the table exceeds A5 page. Consider print on A4 receipt instead.";
+		return;
+	}
+	payment.PaymentDetail.forEach((item) => {
+		const row = document.createElement("tr");
+		row.innerHTML = `<td class="itemId">${item.Item.id}</td><td>${item.Item.name}</td><td class="itemPrice">${item.price}</td>`;
+		document.getElementById("payment-details").appendChild(row);
+	});
+});
