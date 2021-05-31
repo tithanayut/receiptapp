6e69c0a4fb0c1d30370f5067b58cf7825c60223a
Thanayut T <b@thanayut.in.th>
2021-05-31 14:26:21 +0700

    Improve A5 receipt style

--- public/html/receipt/js/receipt.js
+++ public/html/receipt/js/receipt.js
@@ -52,13 +52,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 	}
 	payment.PaymentDetail.forEach((item) => {
 		const row = document.createElement("tr");
-		row.innerHTML = `<td class="itemId">${item.Item.id}</td><td>${item.Item.name}</td><td class="itemPrice">${item.price}</td>`;
+		row.innerHTML = `<td class="itemId">${item.Item.id}</td><td>${
+			item.Item.name
+		}</td><td class="itemPrice">${item.price.toFixed(2)}</td>`;
 		document.getElementById("payment-details").appendChild(row);
 	});
 
 	// Fill total amt.
 	const row = document.createElement("tr");
-	row.innerHTML = `<td colspan="2" style="text-align:right">Total Amount</td><td style="text-align:center">${payment.sum.price.toFixed(
+	row.innerHTML = `<td colspan="2" id="payment-total-label">Total Amount</td><td id="payment-total">${payment.sum.price.toFixed(
 		2
 	)}</td>`;
 	document.getElementById("payment-details").appendChild(row);
